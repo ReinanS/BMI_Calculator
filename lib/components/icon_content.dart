1df7bf255873dd8d138a2d51740011e7806cdf0b
reinanS <reinandesouza01@gmail.com>
2021-03-06 15:36:42 -0300

    feat: criacao do arquivo icon_content

---
+++ lib/components/icon_content.dart
@@ -0,0 +1,32 @@
+import 'package:calculadora_imc/constants.dart';
+import 'package:flutter/material.dart';
+
+class IconContent extends StatelessWidget {
+  IconContent({
+    this.icon,
+    this.label,
+  });
+
+  final String label;
+  final IconData icon;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Icon(
+          icon,
+          size: 60,
+        ),
+        SizedBox(
+          height: 15,
+        ),
+        Text(
+          label,
+          style: kLabelTextStyle,
+        )
+      ],
+    );
+  }
+}

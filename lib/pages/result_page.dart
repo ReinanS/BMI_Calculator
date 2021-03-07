6e102602ead99275b506f7e18f5db83bdce32340
reinanS <reinandesouza01@gmail.com>
2021-03-07 11:42:59 -0300

    feat: criacao do result_page

---
+++ lib/pages/result_page.dart
@@ -0,0 +1,63 @@
+import 'package:calculadora_imc/components/bottombm_button.dart';
+import 'package:calculadora_imc/components/reusable_card.dart';
+import 'package:calculadora_imc/constants.dart';
+import 'package:flutter/material.dart';
+
+class ResultPage extends StatelessWidget {
+  ResultPage({this.bmi, this.result, this.interpretation});
+
+  final String bmi;
+  final String result;
+  final String interpretation;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('IMC'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Expanded(
+            child: Text(
+              'Seu Resultado',
+              style: TextStyle(fontSize: 50, fontWeight: FontWeight.bold),
+            ),
+          ),
+          Expanded(
+            flex: 5,
+            child: ReusableCard(
+              colour: kActiveCardColor,
+              cardChild: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  Text(
+                    result,
+                    textAlign: TextAlign.center,
+                    style: TextStyle(color: Colors.green, fontSize: 30),
+                  ),
+                  Text(
+                    bmi,
+                    textAlign: TextAlign.center,
+                    style: TextStyle(fontSize: 50, color: Colors.white),
+                  ),
+                  Text(
+                    interpretation,
+                    textAlign: TextAlign.center,
+                    style: kLabelTextStyle,
+                  )
+                ],
+              ),
+            ),
+          ),
+          BottomButton(
+              text: 'Re-Calcular',
+              onPress: () {
+                Navigator.pop(context);
+              })
+        ],
+      ),
+    );
+  }
+}

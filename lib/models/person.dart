70ea343f79c2be0218ca8770c51090c7a504512b
reinanS <reinandesouza01@gmail.com>
2021-03-09 17:26:03 -0300

    feat: adiciona gets e sets

--- lib/models/person.dart
+++ lib/models/person.dart
@@ -1,5 +1,42 @@
-class Pessoa {
-  double weight;
-  double height;
-  bool gender;
+class Person {
+  String _nome;
+  double _height;
+  double _weight;
+  Gender _gender;
+
+  Person(this._height, this._weight, this._gender);
+
+  String get nome {
+    return _nome;
+  }
+
+  set nome(String nome) {
+    this._nome = nome;
+  }
+
+  double get height {
+    return _height;
+  }
+
+  set height(double height) {
+    this._height = height;
+  }
+
+  double get weight {
+    return _weight;
+  }
+
+  set weight(double weight) {
+    this._weight = weight;
+  }
+
+  Gender get gender {
+    return _gender;
+  }
+
+  set gender(Gender gender) {
+    this._gender = gender;
+  }
 }
+
+enum Gender { male, female }
